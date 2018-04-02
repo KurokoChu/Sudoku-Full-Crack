0f47ea9de16ae49fde0d065205eb42c7ea718981
Supakit Rodthong <ktutora@gmail.com>
2018-04-03 07:29:25 +0900

    Renew Main Solver function

--- src/solve/main.c
+++ src/solve/main.c
@@ -7,9 +7,9 @@
 #include "IntersectionsTechnique.h"
 
 void Score_Summary();
-int Solve_Board(int **board, int row, int col, int size_r, int size_c);
-int DoStep(int **board, int row, int col, int size_r, int size_c);
-int GetStep(int **board, int row, int col);
+int Solve_Board(int **board, int row, int col);
+int GetStep(int **board, int step);
+int DoStep(int **board, int row, int col, int step);
 
 int main() {
     /* Main function */
@@ -18,19 +18,14 @@ int main() {
     sudoku = Init_Board(size_r, size_c);
     if (IsValid_Board(sudoku, size_r, size_c)) {
         printf("\nPlaying . . .\n\n");
-        if (Solve_Board(sudoku, 0, 0, size_r, size_c) is True) {
-            if (Find_EmptySlot(sudoku, 0, 0) is False) {
-                if (IsValid_Board(sudoku, size_r, size_c) is True) {
-                    printf("\nFinish!\n");
-                    Show_Board(sudoku, size_r, size_c);
-                    Score_Summary();
-                }
-                else {
-                    Show_Board(sudoku, size_r, size_c);
-                }
+        if (Solve_Board(sudoku, 0, 0) isnot -1) {
+            if (IsValid_Board(sudoku, size_r, size_c) is True && Find_EmptySlot(sudoku, 0, 0) is False) {
+                printf("\nFinish!\n");
+                Show_Board(sudoku, size_r, size_c);
+                Score_Summary();
             }
             else {
-                printf("I need to practice more\n");
+                printf("Need more Technique to solve\n");
                 Show_Board(sudoku, size_r, size_c);
             }
         }
@@ -46,110 +41,114 @@ int main() {
     return 0;
 }
 
-int Solve_Board(int **board, int row, int col, int size_r, int size_c) {
+int Solve_Board(int **board, int row, int col) {
     /* Find the solution */
-    for (int i = 0; i < size_r; ++i) {
-        for (int j = 0; j < size_c; ++j) {
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
             Update_Board(board, i, j);
         }
     }
-    if (Find_EmptySlot(board, row, col) is False) {
+    if (Find_EmptySlot(board, 0, 0) is False) {
         return True;
     }
-    return DoStep(board, coord.x, coord.y, size_r, size_c);
+    return GetStep(board, 1);
 }
 
-int DoStep(int **board, int row, int col, int size_r, int size_c) {
-    int next_row, next_col;
-    next_row = 0;
-    next_col = 0;
-    switch(GetStep(board, row, col)) {
-        case 1:
-            printf("Full House : r%dc%d = %d\n", row + 1, col + 1, board[row][col]);
-            score[0].arr[0]++;
-            return Solve_Board(board, next_row, next_col, size_r, size_c);
-        case 2:
-            printf("Naked Single : r%dc%d = %d\n", row + 1, col + 1, board[row][col]);
-            score[1].arr[0]++;
-            return Solve_Board(board, next_row, next_col, size_r, size_c);
-        case 3:
-            printf("Hidden Single : r%dc%d = %d\n", row + 1, col + 1, board[row][col]);
-            score[2].arr[0]++;
-            return Solve_Board(board, next_row, next_col, size_r, size_c);
-        case 4:
-            printf("Locked Pair : [ ");
-            Show_ArrayElement_1D(cell[row][col].arr, 9);
-            printf("] in r%dc%d and r%dc%d\n", coord_pair.x1 + 1, coord_pair.y1 + 1, coord_pair.x2 + 1, coord_pair.y2 + 1);
-            score[3].arr[0]++;
-            next_row = (col + 1 == 9) ? row + 1: row;
-            next_col = (col + 1) % 9;
-            return Solve_Board(board, next_row, next_col, size_r, size_c);
-        case 5:
-            printf("Locked Triple : [ ");
-            Show_ArrayElement_1D(triple.arr, 9);
-            printf("] in r%dc%d, r%dc%d and r%dc%d\n",
-                coord_pair.x1 + 1, coord_pair.y1 + 1, coord_pair.x2 + 1, coord_pair.y2 + 1, coord_pair.x3 + 1, coord_pair.y3 + 1);
-            score[4].arr[0]++;
-            next_row = (col + 1 == 9) ? row + 1: row;
-            next_col = (col + 1) % 9;
-            return Solve_Board(board, next_row, next_col, size_r, size_c);
-        case 6:
-            printf("Locked Candidates Type 1 \"Pointing\" : [ %d ] in r%dc%d and r%dc%d\n", coord_pair.arr[0], coord_pair.x1 + 1, coord_pair.y1 + 1, coord_pair.x2 + 1, coord_pair.y2 + 1);
-            score[5].arr[0]++;
-            next_row = (col + 1 == 9) ? row + 1: row;
-            next_col = (col + 1) % 9;
-            return Solve_Board(board, next_row, next_col, size_r, size_c);
-        case 7:
-            if (coord_pair.num is 1) {
-                printf("Locked Candidates Type 2 \"Claiming\" : [ %d ] in r%dc%d and r%dc%d\n", 
-                    coord_pair.arr[0], coord_pair.x1 + 1, coord_pair.y1 + 1, coord_pair.x2 + 1, coord_pair.y2 + 1);
-            }
-            else if (coord_pair.num is 2) {
-                printf("Locked Candidates Type 2 \"Claiming\" : [ %d ] in r%dc%d, r%dc%d and r%dc%d\n", 
-                    coord_pair.arr[0], coord_pair.x1 + 1, coord_pair.y1 + 1, coord_pair.x2 + 1, coord_pair.y2 + 1, coord_pair.x3 + 1, coord_pair.y3 + 1);
-            }
-            score[6].arr[0]++;
-            next_row = (col + 1 == 9) ? row + 1: row;
-            next_col = (col + 1) % 9;
-            return Solve_Board(board, next_row, next_col, size_r, size_c);
-        default:
-            next_row = (col + 1 == 9) ? row + 1: row;
-            next_col = (col + 1) % 9;
-            return Solve_Board(board, next_row, next_col, size_r, size_c);
+int GetStep(int **board, int step) {
+    if (step > 8) {
+        return False;
     }
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            if (board[i][j] is EmptySlot) {
+                coord.x = i;
+                coord.y = j;
+                if (DoStep(board, i, j, step) is True) {
+                    return Solve_Board(board, 0, 0);
+                }
+            }
+        }
+    }
+    return GetStep(board, step + 1);
 }
 
-int GetStep(int **board, int row, int col) {
+int DoStep(int **board, int row, int col, int step) {
     eliminated = False;
-    if (Find_FullHouse(board, row, col) is True) {
-        (score[0].num is False) ? score[0].num = True: 1;
-        return 1;
+    switch(step) {
+        case 1:
+            if (Find_FullHouse(board, row, col) is True) {
+                (score[0].num is False) ? score[0].num = True: 1;
+                score[0].arr[0]++;
+                printf("Full House : r%dc%d = %d\n", row + 1, col + 1, board[row][col]);
+                return True;
+            }
+            break;
+        case 2:
+            if (Find_NakedSingle(board, row, col) is True) {
+                (score[1].num is False) ? score[1].num = True: 1;
+                score[1].arr[0]++;
+                printf("Naked Single : r%dc%d = %d\n", row + 1, col + 1, board[row][col]);
+                return True;
+            }
+            break;
+        case 3:
+            if (Find_HiddenSingle(board, row, col) is True) {
+                (score[2].num is False) ? score[2].num = True: 1;
+                score[2].arr[0]++;
+                printf("Hidden Single : r%dc%d = %d\n", row + 1, col + 1, board[row][col]);
+                return True;
+            }
+            break;
+        case 4:
+            if (Find_LockedPair(board, row, col) is True) {
+                (score[3].num is False) ? score[3].num = True: 1;
+                score[3].arr[0]++;
+                printf("Locked Pair : [ ");
+                Show_ArrayElement_1D(cell[row][col].arr, 9);
+                printf("] in r%dc%d and r%dc%d\n", coord_pair.x1 + 1, coord_pair.y1 + 1, 
+                                                   coord_pair.x2 + 1, coord_pair.y2 + 1);
+                return True;
+            }
+            break;
+        case 5:
+            if (Find_LockedTriple(board, row, col) is True) {
+                (score[4].num is False) ? score[4].num = True: 1;
+                score[4].arr[0]++;
+                printf("Locked Triple : [ ");
+                Show_ArrayElement_1D(triple.arr, 9);
+                printf("] in r%dc%d, r%dc%d and r%dc%d\n", coord_pair.x1 + 1, coord_pair.y1 + 1, 
+                                                           coord_pair.x2 + 1, coord_pair.y2 + 1, 
+                                                           coord_pair.x3 + 1, coord_pair.y3 + 1);
+                return True;
+            }
+            break;
+        case 6:
+            if (Find_LockedCandidates_1(board, row, col) is True) {
+                (score[5].num is False) ? score[5].num = True: 1;
+                score[5].arr[0]++;
+                printf("Locked Candidates Type 1 \"Pointing\" : [ %d ] in r%dc%d and r%dc%d\n", coord_pair.arr[0],
+                                                                                                coord_pair.x1 + 1, coord_pair.y1 + 1, 
+                                                                                                coord_pair.x2 + 1, coord_pair.y2 + 1);
+                return True;
+            }
+            break;
+        case 7:
+            if (Find_LockedCandidates_2(board, row, col) is True) {
+                (score[6].num is False) ? score[6].num = True: 1;
+                score[6].arr[0]++;
+                printf("Locked Candidates Type 2 \"Claiming\" : [ %d ] in ", coord_pair.arr[0]);
+                (coord_pair.num is 1) ? printf("r%dc%d and r%dc%d\n", 
+                                        coord_pair.x1 + 1, coord_pair.y1 + 1, 
+                                        coord_pair.x2 + 1, coord_pair.y2 + 1):
+                                    printf("r%dc%d, r%dc%d and r%dc%d\n", 
+                                        coord_pair.x1 + 1, coord_pair.y1 + 1, 
+                                        coord_pair.x2 + 1, coord_pair.y2 + 1, 
+                                        coord_pair.x3 + 1, coord_pair.y3 + 1);
+                return True;
+            }
+            break;
+        default: return False;
     }
-    else if (Find_NakedSingle(board, row, col) is True) {
-        (score[1].num is False) ? score[1].num = True: 1;
-        return 2;
-    }
-    else if (Find_HiddenSingle(board, row, col) is True) {
-        (score[2].num is False) ? score[2].num = True: 1;
-        return 3;
-    }
-    else if (Find_LockedPair(board, row, col) is True && eliminated is True) {
-        (score[3].num is False) ? score[3].num = True: 1;
-        return 4;
-    }
-    else if (Find_LockedTriple(board, row, col) is True && eliminated is True) {
-        (score[4].num is False) ? score[4].num = True: 1;
-        return 5;
-    }
-    else if (Find_LockedCandidates_1(board, row, col) is True && eliminated is True) {
-        (score[5].num is False) ? score[5].num = True: 1;
-        return 6;
-    }
-    else if (Find_LockedCandidates_2(board, row, col) is True && eliminated is True) {
-        (score[6].num is False) ? score[6].num = True: 1;
-        return 7;
-    }
-    return 0;
 }
 
 void Score_Summary() {
